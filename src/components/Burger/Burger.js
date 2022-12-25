1b1bbbf9feb194a4d61d2e02a0095bd5b5ffb0e5
poisonivy-dev <melodyiram09@gmail.com>
2022-12-25 23:25:22 +0500

    Adding Ingredients Dynamically

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient type={igKey} key={igKey + i} />;
+    });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/auxiliary";
-
+import Burger from "../Burger/Burger";
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 3,
+      meat: 2,
+    },
+  };
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger Controls</div>
       </Aux>
     );
